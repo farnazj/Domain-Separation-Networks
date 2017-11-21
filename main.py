c4bbc3ff7bcb8b1100f9152f535cbf3401ddef53
Farnaz Jahanbakhsh <farnaz.jahanbakhsh92@gmail.com>
2017-11-20 23:58:12 -0500

    LSTM updated, CNN added, both incomplete

---
+++ main.py
@@ -0,0 +1,64 @@
+import argparse
+import sys
+from os.path import dirname, realpath
+sys.path.append(dirname(dirname(realpath(__file__))))
+import data.dataset_utils as data_utils
+import models.model_utils as model_utils
+import train.train_utils as train_utils
+import os
+import torch
+import datetime
+import cPickle as pickle
+import pdb
+
+
+
+parser = argparse.ArgumentParser(description='PyTorch Example Sentiment Classifier')
+# learning
+parser.add_argument('--lr', type=float, default=0.001, help='initial learning rate [default: 0.001]')
+parser.add_argument('--epochs', type=int, default=256, help='number of epochs for train [default: 256]')
+parser.add_argument('--batch_size', type=int, default=16, help='batch size for training [default: 64]')
+# data loading
+parser.add_argument('--num_workers', nargs='?', type=int, default=4, help='num workers for data loader')
+# model
+parser.add_argument('--model_name', nargs="?", type=str, default='lstm', help="Form of model, i.e dan, rnn, etc.")
+# device
+parser.add_argument('--cuda', action='store_true', default=False, help='enable the gpu')
+parser.add_argument('--train', action='store_true', default=False, help='enable train')
+parser.add_argument('--test', action='store_true', default=False, help='enable test')
+# task
+parser.add_argument('--snapshot', type=str, default=None, help='filename of model snapshot to load[default: None]')
+parser.add_argument('--save_path', type=str, default="model.pt", help='Path where to dump model')
+
+
+
+args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    # update args and print
+
+    print("\nParameters:")
+    for attr, value in sorted(args.__dict__.items()):
+        print("\t{}={}".format(attr.upper(), value))
+
+    train_data, dev_data, embeddings = data_utils.load_dataset(args)
+
+    # model
+    if args.snapshot is None:
+        model = model_utils.get_model(embeddings, args)
+    else :
+        print('\nLoading model from [%s]...' % args.snapshot)
+        try:
+            model = torch.load(args.snapshot)
+        except :
+            print("Sorry, This snapshot doesn't exist."); exit()
+    print(model)
+
+    #print()
+    # train
+    if args.train :
+        train_utils.train_model(train_data, dev_data, model, args)
+    if args.test:
+        pass
+        #train_utils.test_model(test_data, model, args)
--- model/model_utils.py
+++ model/model_utils.py
@@ -14,7 +14,7 @@ def get_model(embeddings, args):
     print("\nBuilding model...")
 
     if args.model_name == 'cnn':
-        return CNN(embeddings, args)
+        return CNN(embeddings, args, feature_size)
     elif args.model_name == 'lstm':
         return LSTM(embeddings, args)
     else:
@@ -24,8 +24,46 @@ def get_model(embeddings, args):
 
 class LSTM(nn.Module):
 
-    def __init__(self, embeddings, args):
+    def __init__(self, embeddings, args, feature_size):
         super(LSTM, self).__init__()
+
+        self.args = args
+        vocab_size, embed_dim = embeddings.shape
+
+        self.embed_dim = embed_dim
+
+        self.embedding_layer = nn.Embedding(vocab_size, embed_dim)
+        self.embedding_layer.weight.data = torch.from_numpy( embeddings )
+
+        self.lstm = nn.LSTM(input_size=feature_size, hidden_size=self.args.hd_size, num_layers=1, batch_first=True, bidirectional=False)
+        #self.W_o = nn.Linear(self.args.hd_size,1)
+
+
+    def forward(self, x_index):
+        #x_index.view(-1,2,sequence_length, feature_size)
+        #x_index.squeeze()
+        titles_indices = x_index[:,1:]
+        bodies_indices = x_index[:,:1]
+
+        #new_batch_size = self.args.batch_size * sequence_length
+
+        input_x_titles = self.embedding_layer(titles_indices)
+        bodies_x_titles = self.embedding_layer(bodies_indices)
+
+        h0 = autograd.Variable(torch.zeros(1, new_batch_size, self.args.hd_size).type(torch.FloatTensor))
+        c0 = autograd.Variable(torch.randn(1, new_batch_size, self.args.hd_size).type(torch.FloatTensor))
+
+        output_title, h_n_title = self.lstm(input_x_titles, (h0, c0))
+        output_body, h_n_body = self.lstm(input_x_bodies, (h0, c0))
+
+        return (output_title + output_body)/2
+
+
+
+class CNN(nn.Module):
+
+    def __init__(self, embeddings, args, feature_size):
+        super(CNN, self).__init__()
 
         self.args = args
         vocab_size, embed_dim = embeddings.shape
@@ -35,17 +73,21 @@ class LSTM(nn.Module):
         self.embedding_layer = nn.Embedding( vocab_size, embed_dim)
         self.embedding_layer.weight.data = torch.from_numpy( embeddings )
 
-        self.lstm = nn.LSTM(input_size=feature_size, hidden_size=self.args.hd_size, num_layers=1, batch_first=True, bidirectional=False)
-        #self.W_o = nn.Linear(self.args.hd_size,1)
+        self.conv1 = nn.Conv1d(feature_size, self.args.hidden_size, kernel_size = 3)
 
+    def forward(self, x_index):
+        #x_indx.view(-1,2,sequence_length, feature_size)
+        #x_indx.squeeze()
+        titles_indices = x_index[:,1:]
+        bodies_indices = x_index[:,:1]
 
-    def forward(self, x_indx):
-        input_x = self.embedding_layer(x_indx)
-        print input_x
-        exit(1)
-        h0 = autograd.Variable(torch.zeros(1, self.args.batch_size, self.args.hd_size).type(torch.FloatTensor))
-        c0 = autograd.Variable(torch.randn(1, self.args.batch_size, self.args.hd_size).type(torch.FloatTensor))
+        input_x_titles = self.embedding_layer(titles_indices)
+        bodies_x_titles = self.embedding_layer(bodies_indices)
 
-        output, h_n = self.lstm(input_x, (h0, c0))
+        #conv receives batch_size * input size* sequence_length (e.g. 16 questions, each having 10 words,
+        #each word having an embedding of size sequence_length)
 
-        return output
+        output_titles = F.max_pool1d(F.tanh(self.conv1(input_x_titles)), 1)
+        output_bodies = F.max_pool1d(F.tanh(self.conv1(input_x_bodies)), 1)
+
+        return (output_title + output_body)/2
