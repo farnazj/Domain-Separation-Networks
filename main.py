02e6754be8c40ef53b31286a1963f7d382b42808
farnazj <farnaz@30-9-167.wireless.csail.mit.edu>
2017-11-25 21:19:38 -0500

    default params changed

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(description='Question Retrieval')
 parser.add_argument('--lr', type=float, default=0.001, help='initial learning rate [default: 0.001]')
 parser.add_argument('--hd_size', type=int, default=200, help='')
 parser.add_argument('--epochs', type=int, default=256, help='number of epochs for train [default: 256]')
-parser.add_argument('--batch_size', type=int, default=16, help='batch size for training [default: 64]')
+parser.add_argument('--batch_size', type=int, default=10, help='batch size for training [default: 64]')
 # data loading
 parser.add_argument('--num_workers', nargs='?', type=int, default=4, help='num workers for data loader')
 # model
