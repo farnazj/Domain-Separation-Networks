99978c0743409b31d7d8ea0a9c5b1fa96a0e222f
N-A-D-I-A <nadiiachepurko@gmail.com>
2017-12-01 17:57:00 -0500

    dataset prepared

--- data/data_utils.py
+++ data/data_utils.py
@@ -6,12 +6,16 @@ import cPickle as pickle
 
 PATH_EMB = "./askubuntu/vector/vectors_pruned.200.txt.gz"
 PATH_TEXT = "./askubuntu/text_tokenized.txt.gz"
-PATH_DEV = "./askubuntu/dev.txt"
-PATH_TEST = "./askubuntu/test.txt"
-PATH_TRAIN = "./askubuntu/train_random.txt"
+
+PATH_ADEV_NEG = "./Android/dev.neg.txt"
+PATH_ADEV_POS = "./Android/dev.pos.txt"
+PATH_ATEST_NEG = "./Android/test.neg.txt"
+PATH_ATEST_POS = "./Android/test.pos.txt"
+
+PATH_ACORP = "./Android/corpus.tsv.gz"
 
 PATH_EMB_SAVE = './embedding.pickle'
-PATH_ID2DATA_SAVE = './id2data.pickle'
+PATH_id2target_SAVE = './id2source.pickle'
 PATH_CONST_SAVE = './consts.txt'
 
 
@@ -32,8 +36,8 @@ def getEmbeddingTensor():
     embedding_tensor = np.array(embedding_tensor, dtype=np.float32)
     return embedding_tensor, word2idx
 
-def getId2Data(word2idx):
-    id2data = {}
+def get_id2source(word2idx):
+    id2source = {}
     max_title = 0
     max_body = 0
 
@@ -59,25 +63,103 @@ def getId2Data(word2idx):
 
 
             if len(title2iarr) != 0 and len(body2iarr) != 0:
-                id2data[qid] = ((title2iarr, len(title2iarr)), (body2iarr, len(body2iarr) ))
+                id2source[qid] = ((title2iarr, len(title2iarr)), (body2iarr, len(body2iarr) ))
 
-    return id2data, max_title, MAX_BODY_LEN
+    return id2source, max_title
 
+def get_id2target(word2idx):
+    id2target = {}
+    max_title = 0
+    max_body = 0
+
+    with gzip.open(PATH_ACORP) as gfile:
+        for line in tqdm.tqdm(gfile):
+            qid, qtitle, qbody = line.split('\t')
+            title = qtitle.split()
+            body = qbody.split()
+
+            title2iarr = [word2idx[x] for x in title if x in word2idx]
+            body2iarr = []
+
+            count = 0
+            for word in body:
+                if word in word2idx:
+                    if count >= MAX_BODY_LEN:
+                        break
+                    body2iarr.append(word2idx[word])
+                    count += 1
+
+            if max_title < len(title2iarr):
+                max_title = len(title2iarr)
+
+
+            if len(title2iarr) != 0 and len(body2iarr) != 0:
+                id2target[qid] = ((title2iarr, len(title2iarr)), (body2iarr, len(body2iarr) ))
+
+    return id2target, max_title
+
+def createTestDic(id2target):
+    dic_test = {}
+
+    with open(PATH_ATEST_POS) as f:
+        for line in tqdm.tqdm(f):
+            first, second= line.split()
+            if (first in id2target) and (second in id2target):
+                dic_test[first] = []
+                dic_test[first].append(second)
+
+    with open(PATH_ATEST_NEG) as f:
+        for line in tqdm.tqdm(f):
+            first, second= line.split()
+            if (first in id2target) and (second in id2target) and (first in dic_test):
+                dic_test[first].append(second)
+
+    return dic_test
+
+def createAndroidDics(id2target):
+    dic_dev = {}
+    domain_question = []
+
+    dic_test = createTestDic(id2target)
+
+    with open(PATH_ADEV_POS) as f:
+        for line in tqdm.tqdm(f):
+            first, second= line.split()
+            if (first in id2target) and (second in id2target):
+                dic_dev[first] = []
+                dic_dev[first].append(second)
+
+    with open(PATH_ADEV_NEG) as f:
+        for line in tqdm.tqdm(f):
+            first, second= line.split()
+            if (first in id2target) and (second in id2target) and (first in dic_dev):
+                dic_dev[first].append(second)
+
+    for q in id2target:
+        if q not in dic_test:
+            domain_question.append(q)
+
+    return dic_dev, dic_test, domain_question
 
 def loadDataset(args):
     print "\nLoading embedding, train, and dev data..."
     embedding_tensor, word2idx = getEmbeddingTensor()
-    id2data, max_title, max_body = getId2Data(word2idx)
 
+    id2source, max_title = get_id2source(word2idx)
+    id2target, max_title2 = get_id2target(word2idx)
+    max_title = max_title2 if max_title2 > max_title else max_title
     args.embedding_dim = embedding_tensor.shape[1]
 
-    train_data = dataset.AskUbuntuDataset(PATH_TRAIN, id2data, max_title, max_body, True)
-    dev_data = dataset.AskUbuntuDataset(PATH_DEV, id2data, max_title, max_body, False)
+    dic_dev, dic_test, domain_question = createAndroidDics(id2target)
 
-    with open(PATH_ID2DATA_SAVE, 'wb') as handle:
-        pickle.dump(id2data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    train_data = dataset.TrainDataset(id2source, id2target, domain_question, max_title, MAX_BODY_LEN)
+
+    dev_data =  dataset.EvalDataset(id2target, dic_dev, max_title, MAX_BODY_LEN)
+
+    with open(PATH_id2target_SAVE, 'wb') as handle:
+        pickle.dump(id2target, handle, protocol=pickle.HIGHEST_PROTOCOL)
     with open(PATH_CONST_SAVE, 'w') as tfile:
-        tfile.write(str(max_title) + '\t' + str(max_body) + '\t' + str(args.embedding_dim))
+        tfile.write(str(max_title) + '\t' + str(MAX_BODY_LEN) + '\t' + str(args.embedding_dim))
 
     return train_data, dev_data, embedding_tensor
 
@@ -85,11 +167,14 @@ def loadDataset(args):
 def loadTest(args):
     print "\nLoading paramters and test data..."
 
-    with open(PATH_ID2DATA_SAVE, 'rb') as handle:
-        id2data = pickle.load(handle)
+    with open(PATH_id2target_SAVE, 'rb') as handle:
+        id2target = pickle.load(handle)
     with open(PATH_CONST_SAVE, 'r') as tfile:
         max_title, max_body, args.embedding_dim = [int(x) for x in tfile.read().split()]
 
-    test_data = dataset.AskUbuntuDataset(PATH_TEST, id2data, max_title, max_body, False)
+    #test data will come from target
+
+    dic_test = createTestDic(id2target)
+    test_data =  dataset.EvalDataset(id2target, dic_test, max_title, MAX_BODY_LEN)
 
     return test_data
--- data/dataset.py
+++ data/dataset.py
@@ -5,6 +5,8 @@ import tqdm
 import torch.utils.data as data
 import random
 
+PATH_TRAIN = "./askubuntu/train_random.txt"
+
 MAX_QCOUNT = 20
 size = 0
 
@@ -27,22 +29,31 @@ def evalSampleDic(q_title, q_body, qt_mask, qb_mask):
     'titles': [q_title],
     'bodies':[q_body],
     'titles_masks':[qt_mask],
-    'bodies_masks':[qb_mask],
-    'similar':[]
+    'bodies_masks':[qb_mask]
     }
     return sample
 
-def trainSampleDic(q_title, p_title, q_body, p_body, qt_mask, pt_mask, qb_mask, pb_mask):
-    sample = {
+
+def trainSampleDic(id2source_list, id2source, id2target,
+                    domain_question, q_title, p_title, q_body,
+                    p_body, qt_mask, pt_mask, qb_mask, pb_mask, max_title, max_body):
+
+    samples = {
     'titles': [q_title, p_title],
     'bodies':[q_body, p_body],
     'titles_masks':[qt_mask, pt_mask],
     'bodies_masks':[qb_mask, pb_mask]
     }
-    return sample
 
-def processCandidate(sample, candidate, id2data, max_title, max_body):
-    (title, t_mask), (body, b_mask) = id2data[candidate]
+    label = random.choice([0,1])
+    if  label == 0:
+        choice = random.choice(id2source_list)
+        (title, t_mask), (body, b_mask) = id2source[choice]
+    else:
+        choice = random.choice(domain_question)
+        while choice not in domain_question:
+            choice = random.choice(domain_question)
+        (title, t_mask), (body, b_mask) = id2target[choice]
 
     t_mask = padmask(t_mask, max_title)
     b_mask = padmask(b_mask, max_body)
@@ -50,10 +61,85 @@ def processCandidate(sample, candidate, id2data, max_title, max_body):
     pad(title, max_title)
     pad(body, max_body)
 
+    question = {
+    'body': [body],
+    'title': [title],
+    'bmask': [b_mask],
+    'tmask': [t_mask],
+    'label': [label]
+    }
+
+    sample = {
+    'samples': samples,
+    'question': question
+    }
+
+    return sample
+
+
+'''
+def trainSampleDic(id2source_list, id2source, id2target,
+                    domain_question, q_title, p_title, q_body,
+                    p_body, qt_mask, pt_mask, qb_mask, pb_mask, max_title, max_body):
+
+    label = random.choice([0,1])
+    if  label == 0:
+        choice = random.choice(id2source_list)
+        (title, t_mask), (body, b_mask) = id2source[choice]
+    else:
+        choice = random.choice(domain_question)
+        while choice not in domain_question:
+            choice = random.choice(domain_question)
+        (title, t_mask), (body, b_mask) = id2target[choice]
+
+    t_mask = padmask(t_mask, max_title)
+    b_mask = padmask(b_mask, max_body)
+
+    pad(title, max_title)
+    pad(body, max_body)
+
+    sample = {
+    'titles': [q_title, p_title],
+    'bodies':[q_body, p_body],
+    'titles_masks':[qt_mask, pt_mask],
+    'bodies_masks':[qb_mask, pb_mask],
+    'dq_body': [body],
+    'dq_title': [title],
+    'dq_bmask': [b_mask],
+    'dq_tmask': [t_mask],
+    'dq_label': [label]
+    }
+
+    return sample
+'''
+
+def processCandidate(sample, candidate, id2target, max_title, max_body, isTrain):
+    (title, t_mask), (body, b_mask) = id2target[candidate]
+
+    t_mask = padmask(t_mask, max_title)
+    b_mask = padmask(b_mask, max_body)
+
+    pad(title, max_title)
+    pad(body, max_body)
+
+
+    if isTrain:
+        sampl = sample['samples']
+    else:
+        sampl = sample
+
+    sampl['titles'].append(title)
+    sampl['bodies'].append(body)
+    sampl['titles_masks'].append(t_mask)
+    sampl['bodies_masks'].append(b_mask)
+
+
+    '''
     sample['titles'].append(title)
     sample['bodies'].append(body)
     sample['titles_masks'].append(t_mask)
     sample['bodies_masks'].append(b_mask)
+    '''
 
 def getCandidate(uset, id2data_list):
     while True:
@@ -62,96 +148,133 @@ def getCandidate(uset, id2data_list):
             break
     return candidate
 
-def fillInTrainSample(sample, p, q, negs, pos, id2data, id2data_list, max_title, max_body):
+def fillInSample(sample, p, q, negs, pos, id2data, id2data_list, max_title, max_body, isTrain):
     count_negs = 0
 
     while count_negs < MAX_QCOUNT-1:
         neg_candidate = None
         if count_negs < len(negs):
             neg_candidate = negs[count_negs]
-        if count_negs >= len(negs) or neg_candidate not in id2data:
+
+        value = id2data.get(neg_candidate)
+
+        if count_negs >= len(negs) or value == None:
             neg_candidate = getCandidate(set().union(negs, pos, [q]), id2data_list)
 
-        processCandidate(sample, neg_candidate, id2data, max_title, max_body)
+        if isTrain:
+            processCandidate(sample, neg_candidate, id2data, max_title, max_body, True)
+        else:
+            processCandidate(sample, neg_candidate, id2data, max_title, max_body, False)
+
         count_negs += 1
 
-def fillInEvalSample(sample, query_q, rest_qs, pos, id2data, id2data_list, max_title, max_body):
+def createSample(q, p, negs, pos, id2source, id2target, id2source_list, id2target_list, domain_question,  max_title, max_body, isTrain):
+    id2data = id2source
 
-    for i, item in enumerate(rest_qs):
-        if item in pos:
-            sample['similar'].append(i)
-        processCandidate(sample, item, id2data, max_title, max_body)
-
-    if len(sample['similar']) < MAX_QCOUNT:
-        while len(sample['similar']) < MAX_QCOUNT:
-            sample['similar'].append(-1)
-
-class AskUbuntuDataset(data.Dataset):
-    def __init__(self, path, id2data, max_title, max_body, isTrain):
-        self.path = path
-        self.dataset = []
-        self.id2data = id2data
-        self.title_dim = max_title
-        self.body_dim = max_body
-        self.id2data_list = list(id2data)
-
-        with open(path) as f:
-            for line in tqdm.tqdm(f):
-                split = line.split('\t')
-                q = split[0]
-                pos = split[1].split()
-                rest_q = split[2].split()
-
-                if isTrain:
-                    negs = [x for x in rest_q if x not in pos]
-
-                    for p in pos:
-                        sample = self.createSample(q, p, negs, pos, max_title, max_body, isTrain)
-                        if sample != None:
-                            self.dataset.append(sample)
-                else:
-                    if len(pos) > 0:
-                        sample = self.createSample(q, -1, rest_q, pos, max_title, max_body, isTrain)
-                    if sample != None:
-                        self.dataset.append(sample)
-
-
-    def createSample(self, q, p, rest_qs, pos, max_title, max_body, isTrain):
-        if q not in self.id2data or (p not in self.id2data and p != -1):
+    if isTrain:
+        if q not in id2source or p not in id2source:
+            return None
+    else:
+        id2data = id2target
+        if q not in id2target or p not in id2target:
             return None
 
-        (q_title, qt_mask), (q_body, qb_mask) = self.id2data[q]
-        qt_mask = padmask(qt_mask, max_title)
-        qb_mask = padmask(qb_mask, max_body)
-        pad(q_title, max_title)
-        pad(q_body, max_body)
+    (q_title, qt_mask), (q_body, qb_mask) = id2data[q]
+    qt_mask = padmask(qt_mask, max_title)
+    qb_mask = padmask(qb_mask, max_body)
+    pad(q_title, max_title)
+    pad(q_body, max_body)
 
-        if isTrain:
-            (p_title, pt_mask), (p_body, pb_mask) = self.id2data[p]
-            pt_mask = padmask(pt_mask, max_title)
-            pb_mask = padmask(pb_mask, max_body)
-            pad(p_title, max_title)
-            pad(p_body, max_body)
-            sample = trainSampleDic(q_title, p_title, q_body, p_body, qt_mask, pt_mask, qb_mask, pb_mask)
-        else:
-            sample = evalSampleDic(q_title, q_body, qt_mask, qb_mask)
+    if isTrain:
+        (p_title, pt_mask), (p_body, pb_mask) = id2data[p]
+        pt_mask = padmask(pt_mask, max_title)
+        pb_mask = padmask(pb_mask, max_body)
+        pad(p_title, max_title)
+        pad(p_body, max_body)
 
-        random.shuffle(rest_qs)
+        sample = trainSampleDic(id2source_list, id2source, id2target,
+                            domain_question, q_title, p_title, q_body,
+                            p_body, qt_mask, pt_mask, qb_mask, pb_mask, max_title, max_body)
+    else:
+        sample = evalSampleDic(q_title, q_body, qt_mask, qb_mask)
 
-        if isTrain == False:
-            random.shuffle(pos)
-            fillInEvalSample(sample, q, rest_qs, pos, self.id2data, self.id2data_list, max_title, max_body)
-        else:
-            fillInTrainSample(sample, p, q, rest_qs, pos, self.id2data, self.id2data_list, max_title, max_body)
+    random.shuffle(negs)
 
+    if isTrain:
+        fillInSample(sample, p, q, negs, pos, id2source, id2source_list, max_title, max_body, isTrain)
+    else:
+        fillInSample(sample, p, q, negs, pos, id2target, id2target_list, max_title, max_body, isTrain)
+
+
+    if isTrain:
+        samples = sample['samples']
+        question = sample['question']
+
+        for key in samples:
+            samples[key] = torch.LongTensor(samples[key])
+        for key in question:
+            question[key] = torch.LongTensor(question[key])
+
+    else:
         for key in sample:
             sample[key] = torch.LongTensor(sample[key])
 
-        return sample
+    '''
+    for key in sample:
+        sample[key] = torch.LongTensor(sample[key])
+
+    '''
+
+    return sample
+
+
+class EvalDataset(data.Dataset):
+    def __init__(self, id2target, dic, max_title, max_body):
+        self.dataset = []
+        self.id2target = id2target
+        self.id2target_list = list(id2target)
+        self.dic = dic
+
+        for key in dic:
+            if len(dic[key]) >= 20:
+                sample = createSample(key, dic[key][0], dic[key][1:], None, None,
+                                      id2target, None, self.id2target_list, None, max_title, max_body, False)
+                if sample != None:
+                    self.dataset.append(sample)
 
     def __len__(self):
         return len(self.dataset)
 
     def __getitem__(self,index):
+        sample = self.dataset[index]
+        return sample
+
+class TrainDataset(data.Dataset):
+    def __init__(self, id2source, id2target, domain_question, max_title, max_body):
+        self.dataset = []
+        self.id2source = id2source
+        self.id2target = id2target
+        self.id2target_list = list(id2target)  #for random pick
+        self.id2source_list = list(id2source)  #for random pick
+
+        with open(PATH_TRAIN) as f:
+            for line in tqdm.tqdm(f):
+                split = line.split('\t')
+                q = split[0]
+                pos = split[1].split()
+                negs = split[2].split()
+
+                for p in pos:
+                    sample = createSample(q, p, negs, pos, id2source, id2target,
+                                        self.id2source_list, self.id2target_list, domain_question,
+                                        max_title, max_body, True)
+                    if sample != None:
+                        self.dataset.append(sample)
+
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
         sample = self.dataset[index]
         return sample
